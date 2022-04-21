369d321898112546e74fdf1f8f6d4859567f2700
boyevivan <boyev.ivan@gmail.com>
2022-04-21 03:12:40 +0300

    add

---
+++ QA_HW_JS/HW_JS_26-33.js
@@ -0,0 +1,14 @@
+
+let age_1 = 10; 
+let age_2 = 18;
+let age_3 = 60;
+
+
+if (age_1 < age_2) {
+console.log('You don’t have access cause your age is ' + age_1 +  ' it less than ' + age_2)
+} 
+else if(age_1 >= age_2 && age_1 < age_3) {
+console.log('Welcome')
+}
+else if(age_1 > age_3) {
+console.log('Keep calm and look Culture channel')}
